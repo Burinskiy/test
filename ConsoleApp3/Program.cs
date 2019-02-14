31a154a7808a90b70d653f17d88fc26eb5737f0d
oleksandrburinskij <oleksandrburinskij@gmail.com>
2019-02-14 17:17:19 +0200

    file

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApp3
 {
@@ -6,7 +7,41 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (FileStream fstream = File.OpenRead(@"1.txt"))
+            {
+                byte[] array = new byte[fstream.Length];
+                fstream.Read(array, 0, array.Length);
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                int k = 0;
+                int maxlen = 0;
+                for (int i =0; i< textFromFile.Length; i++)
+                {
+                    if (textFromFile[i] == '1' && i != textFromFile.Length - 1)
+                    {
+                        k++;
+                    }
+                    else if (i==textFromFile.Length-1 && textFromFile[i]=='1')
+                    {
+                        k++;
+                        if (maxlen < k)
+                        {
+                            maxlen = k;
+                            k = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (maxlen < k)
+                        {
+                            maxlen = k;
+                        }
+                        k = 0;
+                    }
+
+                }
+                Console.WriteLine(maxlen);
+            }
+            Console.ReadLine();
         }
     }
 }
